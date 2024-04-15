86f5998e5e3fdb8b232e8daee68433b0d58435e8
BigGafLeo <karoldzwon@gmail.com>
2024-04-15 11:11:59 +0200

    App.js

--- models/product.js
+++ models/product.js
@@ -1,6 +1,6 @@
 const db = require("../util/database");
 
-const Cart = require("../cart");
+const Cart = require("./cart");
 
 module.exports = class Product {
   constructor(id, title, imageUrl, description, price) {
