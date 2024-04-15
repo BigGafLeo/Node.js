669aeb14f38ed0c08ab0698273c4dc8cab6dfb94
BigGafLeo <karoldzwon@gmail.com>
2024-04-15 18:43:15 +0200

    Nowa baza sql

--- models/cart.js
+++ models/cart.js
@@ -1,20 +1,27 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const p = path.join(path.dirname(require.main.filename), "data", "cart.json");
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'cart.json'
+);
 
 module.exports = class Cart {
   static addProduct(id, productPrice) {
+    // Fetch the previous cart
     fs.readFile(p, (err, fileContent) => {
       let cart = { products: [], totalPrice: 0 };
       if (!err) {
         cart = JSON.parse(fileContent);
       }
+      // Analyze the cart => Find existing product
       const existingProductIndex = cart.products.findIndex(
-        (prod) => prod.id === id
+        prod => prod.id === id
       );
       const existingProduct = cart.products[existingProductIndex];
       let updatedProduct;
+      // Add new product/ increase quantity
       if (existingProduct) {
         updatedProduct = { ...existingProduct };
         updatedProduct.qty = updatedProduct.qty + 1;
@@ -25,7 +32,7 @@ module.exports = class Cart {
         cart.products = [...cart.products, updatedProduct];
       }
       cart.totalPrice = cart.totalPrice + +productPrice;
-      fs.writeFile(p, JSON.stringify(cart), (err) => {
+      fs.writeFile(p, JSON.stringify(cart), err => {
         console.log(err);
       });
     });
@@ -36,18 +43,19 @@ module.exports = class Cart {
       if (err) {
         return;
       }
-      const updateCart = { ...JSON.parse(fileContent) };
-      const product = updateCart.products.find((prod) => prod.id === id);
+      const updatedCart = { ...JSON.parse(fileContent) };
+      const product = updatedCart.products.find(prod => prod.id === id);
       if (!product) {
-        return;
+          return;
       }
       const productQty = product.qty;
-      updateCart.products = updateCart.products.filter(
-        (prod) => prod.id !== id
+      updatedCart.products = updatedCart.products.filter(
+        prod => prod.id !== id
       );
-      updateCart.totalPrice = updateCart.totalPrice - productPrice * productQty;
+      updatedCart.totalPrice =
+        updatedCart.totalPrice - productPrice * productQty;
 
-      fs.writeFile(p, JSON.stringify(updateCart), (err) => {
+      fs.writeFile(p, JSON.stringify(updatedCart), err => {
         console.log(err);
       });
     });
--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("node-complete", "root", "1Piotrek", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "nodecomplete",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
