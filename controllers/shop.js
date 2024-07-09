e2ca56daa0bbdefd4589418685edf715b2dc4f49
BigGafLeo <karoldzwon@gmail.com>
2024-07-09 13:06:09 +0200

    Dodanie_platnosci_nie_dziala_w_pelni

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const stripe = require("stripe")('sk_test_51PaZQ5RvJEhVm7lhLmRpOPSqWB2hT0TK2tX0fzKohgQQil1j488Nu4EapBziWBmzIDhpztDEWdn2Jji6g5SdMRcF00yMuk2AM9')
 
 const PDFDocument = require("pdfkit");
 
@@ -139,7 +140,57 @@ exports.postCartDeleteProduct = (req, res, next) => {
     });
 };
 
-exports.postOrder = (req, res, next) => {
+exports.getCheckout = (req, res, next) => {
+  let products;
+  let total = 0;
+  req.user
+    .populate('cart.items.productId')
+    .then((user) => {
+      products = user.cart.items;
+      total = 0;
+      products.forEach((p) => {
+        total += p.quantity * p.productId.price;
+      });
+
+      return stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: products.map((p) => {
+          return {
+            price_data: {
+              currency: 'pln',
+              product_data: {
+                name: p.productId.title,
+                description: p.productId.description,
+              },
+              unit_amount: p.productId.price * 100, // Kwota w groszach
+            },
+            quantity: p.quantity,
+          };
+        }),
+        mode: 'payment',
+        success_url: req.protocol + '://' + req.get('host') + '/checkout/success',
+        cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel',
+      });
+    })
+    .then((session) => {
+      console.log('Stripe session created:', session);
+      res.render('shop/checkout', {
+        path: '/checkout',
+        pageTitle: 'Checkout',
+        products: products,
+        totalSum: total,
+        sessionId: session.id,
+      });
+    })
+    .catch((err) => {
+      console.error('Stripe error:', err);
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
   req.user
     .populate("cart.items.productId")
     //.execPopulate()
